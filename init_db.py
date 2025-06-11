7e21dc0566609f879cdf99dfdfd25e4b69684256
50nk31 <edwartatewa@gmail.com>
2025-06-12 00:37:50 +0300

    Add files via upload

---
+++ init_db.py
@@ -0,0 +1,10 @@
+    from app import app, db, User
+
+with app.app_context():
+    db.create_all()
+    if not User.query.filter_by(username='admin').first():
+        admin = User(username='admin')
+        admin.set_password('admin')
+        db.session.add(admin)
+        db.session.commit()
+    print("Database initialized and admin user created (if needed)")
